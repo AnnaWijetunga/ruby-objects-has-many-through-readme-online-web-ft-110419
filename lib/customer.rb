4f8f2b8ca6a9127110d98ced977aebbdc305274a
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-20 19:30:48 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -19,10 +19,10 @@ class Customer
   end
   
   def meals
-  Meal.all.select do |meal|
-    meal.customer == self
-  end
-end
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+ end
   
   
 end
